8a54836fecfe315dc10aecc4d5d72d68a71f57fd
Rafael Xavier <78372916+rafaelxvr@users.noreply.github.com>
2023-08-26 13:40:33 -0300

    feat: navigation component

--- src/view/src/main.js
+++ src/view/src/main.js
@@ -1,11 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './style.css'
-import router from './router/index.js'
+import router from './router'
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
+const app = createApp(App)
 
-createApp(App)
-    .use(router)
-    .mount('#app')
+app.use(router)
+
+app.mount('#app')
--- src/view/src/router/index.js
+++ src/view/src/router/index.js
@@ -1,25 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import ClientView from '../views/ClientView.vue'
+import Home from '../views/Home.vue'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: ClientView
-        },
-        {
-            path: '/add',
-            name: 'add',
-            component: () => import('../views/AddClient.vue')
-        },
-        {
-            path: '/edit/:id',
-            name: 'edit',
-            component: () => import('../views/UpdateClient.vue')
-        }
-    ]
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    }
+  ]
 })
 
 export default router
